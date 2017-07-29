0f63dcd6bc13c2409b6cf7f283df9e380b24e220
selvapriya33 <30342331+selvapriya33@users.noreply.github.com>
2017-07-29 16:28:24 +0530

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+char s[21];
+int n,i;
+printf("value");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+sacnf("%c",s[i]);
+}
+for(i=0;i<n;i++)
+{
+printf("%c",s[i]);
+}
+}
