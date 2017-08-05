fdfc857b2d9fff84827900bb3f7f9fc260dc338c
selvapriya33 <30342331+selvapriya33@users.noreply.github.com>
2017-08-05 16:27:57 +0530

    Create ser.c

---
+++ ser.c
@@ -0,0 +1,10 @@
+    #include<stdio.h>
+    void main() 
+    {
+    int *a, *b;
+    int num;
+    printf("\nEnter two numbers : ");
+    scanf("%d %d", &a, &b);
+    num = *ptr1 + *ptr2;
+    printf("Sum = %d", num);
+    }
