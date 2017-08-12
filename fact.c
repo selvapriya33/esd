65e4c697c91f91c7ce7428bde7418c62f78ed19a
selvapriya33 <30342331+selvapriya33@users.noreply.github.com>
2017-08-12 16:45:30 +0530

    Create fact.c

---
+++ fact.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void main()
+{
+    int n, i;
+    int factorial = 1;
+
+    printf("Enter an integer: ");
+    scanf("%d",&n);
+    if (n < 0)
+        printf("Error! Factorial of a negative number doesn't exist.");
+
+    else
+    {
+        for(i=1; i<=n; ++i)
+        {
+            factorial *= i;              
+        }
+        printf("Factorial of %d = %d", n, factorial);
+    }
+
+    
+}
